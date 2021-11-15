3346d3a5388eab637acc15120afe5d81dab7a4d0
odmin <dizz74@mail.ru>
2021-11-15 22:56:56 +0500

    Часть 2 + Часть 3

---
+++ Part2/Part2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int positiveCount = 0, negativeCount = 0;
+            do
+            {
+                int input;
+                Console.WriteLine("Введите число:");
+                input = Convert.ToInt32(Console.ReadLine());
+                if (input == 0) break;
+
+                if (input > 0) positiveCount++;
+                else negativeCount++;
+
+                if (positiveCount == negativeCount) Console.WriteLine("Одинаковое количество положительных и отрицательных");
+                else
+                    Console.WriteLine("Положительных чисел " + ((positiveCount > negativeCount) ? "больше" : "меньше") + ",чем отрицательных");
+
+            }
+            while (true);
+            Console.WriteLine("Окончание");
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Part3/Part3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Part3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите А:");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите B:");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите C:");
+            int C = Convert.ToInt32(Console.ReadLine());
+            if (A < 1 || B < 1 || C < 1) Console.WriteLine("Ошибка ввода");
+            else if(A < C || B < C) Console.WriteLine("Ошибка ввода: сторона квадрата больше, чем прямоугольник, ответ 0");
+            else
+            {
+                int countTotal = 0;
+                int tempA= A;
+
+                while (tempA >= C)
+                {
+                    tempA -= C;
+                    int tempB = B;
+                    while (tempB >= C)
+                    {
+                        tempB -= C;
+                        countTotal++;
+                    }
+                }
+                Console.WriteLine("Итого в прямоугольнике {0}x{1} помещается {2} квадратов(а) {3}x{3}", A,B,countTotal,C);
+            }
+            Console.ReadKey();
+        }
+    }
+}
