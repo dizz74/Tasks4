9e1ed2ab258575c9b37c2418aedd0e22f1cfb714
odmin <dizz74@mail.ru>
2021-11-18 19:21:46 +0500

    fix fields name

--- Part3/Program.cs
+++ Part3/Program.cs
@@ -7,29 +7,29 @@ namespace Part3
         static void Main(string[] args)
         {
             Console.WriteLine("Введите А:");
-            int A = Convert.ToInt32(Console.ReadLine());
+            int nA = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите B:");
-            int B = Convert.ToInt32(Console.ReadLine());
+            int nB = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Введите C:");
-            int C = Convert.ToInt32(Console.ReadLine());
-            if (A < 1 || B < 1 || C < 1) Console.WriteLine("Ошибка ввода");
-            else if(A < C || B < C) Console.WriteLine("Ошибка ввода: сторона квадрата больше, чем прямоугольник, ответ 0");
+            int nC = Convert.ToInt32(Console.ReadLine());
+            if (nA < 1 || nB < 1 || nC < 1) Console.WriteLine("Ошибка ввода");
+            else if(nA < nC || nB < nC) Console.WriteLine("Ошибка ввода: сторона квадрата больше, чем прямоугольник, ответ 0");
             else
             {
                 int countTotal = 0;
-                int tempA= A;
+                int tempA= nA;
 
-                while (tempA >= C)
+                while (tempA >= nC)
                 {
-                    tempA -= C;
-                    int tempB = B;
-                    while (tempB >= C)
+                    tempA -= nC;
+                    int tempB = nB;
+                    while (tempB >= nC)
                     {
-                        tempB -= C;
+                        tempB -= nC;
                         countTotal++;
                     }
                 }
-                Console.WriteLine("Итого в прямоугольнике {0}x{1} помещается {2} квадратов(а) {3}x{3}", A,B,countTotal,C);
+                Console.WriteLine("Итого в прямоугольнике {0}x{1} помещается {2} квадратов(а) {3}x{3}", nA,nB,countTotal,nC);
             }
             Console.ReadKey();
         }
