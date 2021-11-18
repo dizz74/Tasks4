797531e5e2f72aa26098f3be29680f3393cac0af
odmin <dizz74@mail.ru>
2021-11-18 19:19:28 +0500

    убрал while (true)

--- Part2/Program.cs
+++ Part2/Program.cs
@@ -6,25 +6,29 @@ namespace Part2
     {
         static void Main(string[] args)
         {
-            
+
             int positiveCount = 0, negativeCount = 0;
+
+            int input;
             do
             {
-                int input;
                 Console.WriteLine("Введите число:");
                 input = Convert.ToInt32(Console.ReadLine());
-                if (input == 0) break;
 
-                if (input > 0) positiveCount++;
-                else negativeCount++;
 
-                if (positiveCount == negativeCount) Console.WriteLine("Одинаковое количество положительных и отрицательных");
-                else
-                    Console.WriteLine("Положительных чисел " + ((positiveCount > negativeCount) ? "больше" : "меньше") + ",чем отрицательных");
+                if (input != 0)
+                {
+                    if (input > 0) positiveCount++;
+                    else negativeCount++;
 
+                    if (positiveCount == negativeCount) 
+                        Console.WriteLine("Одинаковое количество положительных и отрицательных");
+                    else
+                        Console.WriteLine("Положительных чисел " + ((positiveCount > negativeCount) ? "больше" : "меньше") + ",чем отрицательных");
+                }
             }
-            while (true);
-            Console.WriteLine("Окончание");
+            while (input != 0);
+            Console.WriteLine("Вы ввели ноль, выход");
             Console.ReadKey();
         }
 
